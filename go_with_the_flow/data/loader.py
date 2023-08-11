359b4cc36dd94d68051d07feae7d13fb0553b583
Gabriella Pizzuto <gabriella.pizzuto@liverpool.ac.uk>
2023-08-11 13:56:51 +0100

    formatted with pep-8 standard

--- go_with_the_flow/data/loader.py
+++ go_with_the_flow/data/loader.py
@@ -3,6 +3,7 @@ import re
 from typing import Tuple, List
 
 import numpy as np
+import torch
 from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from sklearn.model_selection import train_test_split
@@ -12,9 +13,7 @@ from ..utils.functions import Dataset3DCNN, labels2cat
 from ..transforms.transform import create_loading_transform
 
 
-def create_reg_datasets(batch_size: int, data_path: str,
-                        selected_frames: List[int],
-                        img_x: int, img_y: int) -> Tuple[DataLoader, DataLoader, DataLoader]:
+def create_reg_datasets(batch_size: int, data_path: str, selected_frames: List[int], img_x: int, img_y: int) -> Tuple[DataLoader, DataLoader, DataLoader]:
     """Create the training, testing and validation data loaders for training
     regression models. Note that the each training video is stored inside a
     folder with the folder named with its corresponding viscosity value.
@@ -25,7 +24,6 @@ def create_reg_datasets(batch_size: int, data_path: str,
         int_x: x dimension to transform the input image for training
         int_y: y dimension to transform the input image for training
     """
-
     all_x_list = []
     all_y_list = []
 
@@ -38,12 +36,10 @@ def create_reg_datasets(batch_size: int, data_path: str,
     validation_size = 0.2
 
     # train, test split
-    train_list, test_list, train_label, test_label = train_test_split(all_x_list, all_y_list,
-                                                                      test_size=0.2, random_state=0)
+    train_list, test_list, train_label, test_label = train_test_split(all_x_list, all_y_list, test_size=0.2, random_state=0)
 
     transform = create_loading_transform(img_x, img_y)
-    train_set = Dataset3DCNN(data_path, train_list, train_label, selected_frames,
-                             transform=transform)
+    train_set = Dataset3DCNN(data_path, train_list, train_label, selected_frames, transform=transform)
     test_set = Dataset3DCNN(data_path, test_list, test_label, selected_frames, transform=transform)
 
     # split into training and validation batches
@@ -57,19 +53,14 @@ def create_reg_datasets(batch_size: int, data_path: str,
     valid_sampler = SubsetRandomSampler(valid_idx)
 
     # loading train, validation and test data
-
-    train_loader = DataLoader(train_set, batch_size=batch_size, sampler=train_sampler,
-                              num_workers=0)
-    valid_loader = DataLoader(train_set, batch_size=batch_size, sampler=valid_sampler,
-                              num_workers=0)
+    train_loader = DataLoader(train_set, batch_size=batch_size, sampler=train_sampler, num_workers=0)
+    valid_loader = DataLoader(train_set, batch_size=batch_size, sampler=valid_sampler, num_workers=0)
     test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=0)
 
     return train_loader, test_loader, valid_loader
 
 
-def create_cls_datasets(batch_size: int, data_path: str,
-                        class_labels: List[str], selected_frames: List[int],
-                        img_x: int, img_y: int) -> Tuple[DataLoader, DataLoader, DataLoader]:
+def create_cls_datasets(batch_size: int, data_path: str, class_labels: List[str], selected_frames: List[int], img_x: int, img_y: int) -> Tuple[DataLoader, DataLoader, DataLoader]:
     """Create the training, testing and validation data loaders for training classification models
     Note that the each training video is stored inside a folder with the folder named with its
     corresponding viscosity value.
@@ -81,15 +72,9 @@ def create_cls_datasets(batch_size: int, data_path: str,
         int_x: x dimension to transform the input image for training
         int_y: y dimension to transform the input image for training
     """
-
     # convert labels -> category
     label_encoder = LabelEncoder()
     label_encoder.fit(class_labels)
-
-    # # convert category -> 1-hot
-    # viscosity_cats = label_encoder.transform(class_labels).reshape(-1, 1)
-    # one_hot_enc = OneHotEncoder()
-    # one_hot_enc.fit(viscosity_cats)
 
     all_x_list = []
     all_y_list = []
@@ -108,12 +93,10 @@ def create_cls_datasets(batch_size: int, data_path: str,
     validation_size = 0.2
 
     # train, test split
-    train_list, test_list, train_label, test_label = train_test_split(all_x_list, all_y_list,
-                                                                      test_size=0.2, random_state=0)
+    train_list, test_list, train_label, test_label = train_test_split(all_x_list, all_y_list, test_size=0.2, random_state=0)
 
     transform = create_loading_transform(img_x, img_y)
-    train_set = Dataset3DCNN(data_path, train_list, train_label, selected_frames,
-                             transform=transform)
+    train_set = Dataset3DCNN(data_path, train_list, train_label, selected_frames, transform=transform)
     test_set = Dataset3DCNN(data_path, test_list, test_label, selected_frames, transform=transform)
 
     # split into training and validation batches
@@ -127,11 +110,8 @@ def create_cls_datasets(batch_size: int, data_path: str,
     valid_sampler = SubsetRandomSampler(valid_idx)
 
     # loading train, validation and test data
-
-    train_loader = DataLoader(train_set, batch_size=batch_size, sampler=train_sampler,
-                              num_workers=0)
-    valid_loader = DataLoader(train_set, batch_size=batch_size, sampler=valid_sampler,
-                              num_workers=0)
+    train_loader = DataLoader(train_set, batch_size=batch_size, sampler=train_sampler, num_workers=0)
+    valid_loader = DataLoader(train_set, batch_size=batch_size, sampler=valid_sampler, num_workers=0)
     test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=0)
 
     return train_loader, test_loader, valid_loader
